be46877983d699a4d0ca461c960e9d1960f9914b
nghe.huavan <nghe.huavan@gmail.com>
2023-08-17 11:47:05 +0700

    .

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname));
+app.use('/plugins', express.static(__dirname + '/plugins'));
 app.listen(port, function () {
   console.log('listening on port http://localhost:' + port);
 });
