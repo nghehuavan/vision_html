790b3e62c1b8a364b7396535f847cd83e32eb412
nghe.huavan <nghe.huavan@gmail.com>
2023-08-17 15:35:41 +0700

    .

--- js/share.js
+++ js/share.js
@@ -2,8 +2,9 @@ $(document).ready(function () {
   var tweetBaseUrl = 'https://twitter.com/intent/tweet?';
   var lineBaseUrl = 'https://social-plugins.line.me/lineit/share?';
   $('a.btn-x').each(function (_, el) {
-    var hashtags = $(el).attr('hashtags');
-    var urlTweet = tweetBaseUrl + 'url=' + encodeURIComponent(window.location.href) + (hashtags ? '&hashtags=' + encodeURIComponent(hashtags.split(' ').join(' #')) : '');
+    var hashtags = $(el).attr('hashtags') ?? '';
+    if (hashtags && hashtags.startsWith('#')) hashtags = hashtags.substring(1);
+    var urlTweet = tweetBaseUrl + 'url=' + encodeURIComponent(window.location.href) + '&hashtags=' + encodeURIComponent(hashtags);
     $(el).attr('href', urlTweet);
   });
   $('a.btn-line').each(function (_, el) {
