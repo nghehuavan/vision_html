a1c4b309aac1349c4b78746a5bf5450a9bda4b34
nghe.huavan <nghe.huavan@gmail.com>
2023-08-17 11:56:25 +0700

    .

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-app.use('/plugins', express.static(__dirname + '/plugins'));
-app.use(express.static(__dirname));
-app.listen(port, function () {
-  console.log('listening on port http://localhost:' + port);
-});
+var liveServer = require("live-server");
+var params = {
+	port: 3000, // Set the server port. Defaults to 8080.
+	host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+	root: "./", // Set root directory that's being served. Defaults to cwd.
+	open: false, // When false, it won't load your browser by default.
+	middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack  
+};
+liveServer.start(params);
