d562b618cbb040f6447a45ec3b76833ab0841519
nghe.huavan <nghe.huavan@gmail.com>
2023-08-17 14:03:19 +0700

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,57 @@
 var gulp = require('gulp');
+const fileinclude = require('gulp-file-include');
 var browserSync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
 
 const paths = {
   src: './',
-  css: './css/',
+  dist: './dist/',
 };
 
-// Static Server + watching scss/html files
+// Static Server
 gulp.task('serve', function () {
   browserSync.init({
-    server: paths.src,
+    server: paths.dist,
   });
 
+  // Watch for build
+  gulp.watch(['./**/*.html'], gulp.series('include-html'));
   gulp.watch(['./sass/**/*.scss'], gulp.series('compile-sass'));
-  gulp.watch(paths.src + '**/*').on('change', browserSync.reload);
+  gulp.watch(['./images/**/*'], gulp.series('copy-images'));
+  gulp.watch(['./js/**/*'], gulp.series('copy-js'));
+  gulp.watch(['./plugins/**/*'], gulp.series('copy-plugins'));
+
+  // Watch for reload page
+  gulp.watch(paths.dist + '**/*').on('change', browserSync.reload);
 });
+
+gulp.task('include-html', function () {
+  return gulp
+    .src(['*.html'])
+    .pipe(fileinclude({ prefix: '@@', basepath: '@file' }))
+    .pipe(gulp.dest(paths.dist));
+});
+
+// copy to dist
+gulp.task('copy-images', function () {
+  return gulp.src(['./images/**/*']).pipe(gulp.dest(paths.dist + 'images'));
+});
+gulp.task('copy-js', function () {
+  return gulp.src(['./js/**/*']).pipe(gulp.dest(paths.dist + 'js'));
+});
+gulp.task('copy-plugins', function () {
+  return gulp.src(['./plugins/**/*']).pipe(gulp.dest(paths.dist + 'plugins'));
+});
+gulp.task('copy-to-dist', gulp.series(gulp.parallel('copy-images', 'copy-js', 'copy-plugins')));
 
 // compile sass into css
 gulp.task('compile-sass', function () {
-  return gulp.src('./sass/**/*.scss').pipe(sass()).pipe(gulp.dest(paths.css));
+  return gulp
+    .src('./sass/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest(paths.dist + 'css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.series('compile-sass', 'serve'));
+gulp.task('build', gulp.series('include-html', 'compile-sass', 'copy-to-dist'));
+gulp.task('default', gulp.series('include-html', 'compile-sass', 'copy-to-dist', 'serve'));
